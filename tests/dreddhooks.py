afa86e1dd52b82e0010c28dbcd3166a18e2d036e
Jonathan Dursi <jonathan@dursi.ca>
2018-08-02 10:18:25 -0400

    POST Response includes object and Location: header
    POSTing an object returns the object, and a Location: header giving the relative link to the object. (TODO: make header absolute).

--- tests/dreddhooks.py
+++ tests/dreddhooks.py
@@ -1,25 +1,54 @@
 import json
 import dredd_hooks as hooks
 
-UUID_EXAMPLE="bf3ba75b-8dfe-4619-b832-31c4a087a589"
+UUID_EXAMPLE = "bf3ba75b-8dfe-4619-b832-31c4a087a589"
 response_stash = {}
 
-@hooks.after("/individuals > Get all individuals > 200 > application/json")
+
+@hooks.after("/v1/individuals > Get all individuals > 200 > application/json")
 def save_individuals_response(transaction):
     parsed_body = json.loads(transaction['real']['body'])
     ids = [item['id'] for item in parsed_body]
     response_stash['individual_ids'] = ids
 
-@hooks.after("/variants > Get all variants within genomic range > 200 > application/json")
+
+@hooks.after("/v1/variants > Get all variants within genomic range > 200 > application/json")
 def save_variants_response(transaction):
+    print("IN SAVE VARIANTS RESPONSE")
     parsed_body = json.loads(transaction['real']['body'])
     ids = [item['id'] for item in parsed_body]
+    print(ids)
     response_stash['variant_ids'] = ids
+    print(response_stash)
 
-@hooks.before("/individuals/{individual_id}/variants > Get variants called in an individual > 200 > application/json")
+
+@hooks.after("/v1/calls > Get all calls > 200 > application/json")
+def save_calls_response(transaction):
+    parsed_body = json.loads(transaction['real']['body'])
+    ids = [item['id'] for item in parsed_body]
+    response_stash['call_ids'] = ids
+
+
+@hooks.before("/v1/individuals/{individual_id}/variants > Get variants called in an individual > 200 > application/json")
+@hooks.before("/v1/individuals/{individual_id} > Get specific individual > 200 > application/json")
 def insert_individual_id(transaction):
     transaction['fullPath'] = transaction['fullPath'].replace(UUID_EXAMPLE, response_stash['individual_ids'][0])
 
-@hooks.before("/variants/{variant_id}/individuals > Get individuals with a given variant called > 200 > application/json")
+
+@hooks.before("/v1/variants/{variant_id}/individuals > Get individuals with a given variant called > 200 > application/json")
+@hooks.before("/v1/variants/{variant_id} > Get specific variant > 200 > application/json")
 def insert_variant_id(transaction):
-    transaction['fullPath'] = transaction['fullPath'].replace(UUID_EXAMPLE, response_stash['variant_ids'][0])
+    if 'variant_ids' in response_stash:
+        transaction['fullPath'] = transaction['fullPath'].replace(UUID_EXAMPLE, response_stash['variant_ids'][0])
+
+
+@hooks.before("/v1/calls/{call_id} > Get specific call > 200 > application/json")
+def insert_vall_id(transaction):
+    transaction['fullPath'] = transaction['fullPath'].replace(UUID_EXAMPLE, response_stash['call_ids'][0])
+
+
+@hooks.before("/v1/individuals/{individual_id} > Get specific individual > 404 > application/json")
+@hooks.before("/v1/variants/{variant_id} > Get specific variant > 404 > application/json")
+@hooks.before("/v1/calls/{call_id} > Get specific call > 404 > application/json")
+def let_pass(transaction):
+    transaction['skip'] = False
